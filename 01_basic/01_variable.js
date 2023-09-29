f7475272193488b9933ce7eea622133ccaac44e7
AbhiCoder108 <145904972+AbhiCoder108@users.noreply.github.com>
2023-09-29 22:31:39 +0000

    variable&datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 1144
+let accountEmail = "ansh@google.com"
+var accountPassword = "12345"
+accountCity = "pune"
+
+
+
+//accountId = 4411  // not allowed 
+
+accountEmail ="bittu@gmail.com"
+accountPassword = "2211"
+accountCity = "patna"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issues in block scope and function scope
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert (3+3)  // we are using nodejs, not browser
+
+
+console.log(3+3) 
+
+console.log("Hitesh")
+
+
+// number => 2 power 52
+//Bigint
+//String => " "
+//boolean => True/False
+//null => standalone Value
+// undefine => value not assign 
+//symbol => uniqe
+
+// object 
+
+
+console.log (typeof "ansh ")
+console.log(typeof undefine); // undefine ...
+console.log(typeof null); // object..
